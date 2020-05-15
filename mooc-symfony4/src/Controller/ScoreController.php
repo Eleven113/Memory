a363e8f6828fb2c4d86718461a354855b63503e0
Thibaut MINARD <minard.thibaut@free.fr>
2020-05-15 14:11:39 +0200

    Start SetScore.js

--- mooc-symfony4/src/Controller/ScoreController.php
+++ mooc-symfony4/src/Controller/ScoreController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Flex\Response;
 
@@ -48,5 +49,13 @@ class ScoreController extends AbstractController
         }
 
         return new JsonResponse($result);
+    }
+
+    /**
+     * @Route("/setscore/", name="app_setscore")
+     */
+    public function setscore(Request $request)
+    {
+        
     }
 }
