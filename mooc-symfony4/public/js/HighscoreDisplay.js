93b64c8a467692a52d7a35145b3fa8bee134c74e
Thibaut MINARD <minard.thibaut@free.fr>
2020-05-15 13:50:19 +0200

    Highscore Display Ok

--- mooc-symfony4/public/js/HighscoreDisplay.js
+++ mooc-symfony4/public/js/HighscoreDisplay.js
@@ -4,6 +4,7 @@ class HighscoreDisplay{
         this.divMedium = document.getElementById("hs_difficulty-medium");
         this.divHard = document.getElementById("hs_difficulty-hard");
         this.h4 = document.getElementById("caption");
+        this.tbody = document.querySelector("tbody");
         this.events();
 
     }
@@ -11,52 +12,62 @@ class HighscoreDisplay{
     events(){
         this.divEasy.addEventListener("click", function(event){
             this.display(event);
-            console.log("click easy");
         }.bind(this));
+
         this.divMedium.addEventListener("click", function(event){
             this.display(event);
-            console.log("click Medium");
         }.bind(this));
+
         this.divHard.addEventListener("click", function(event){
             this.display(event);
-            console.log("click Hard");
         }.bind(this));
 
     }
 
     display(event){
         this.difficulty = event.target.id.split('-')[1];
-        //
-        // this.difficulty = this.difficulty.charAt(0).toUpperCase() + this.difficulty.slice(1);
-        console.log(this.difficulty);
-
 
         switch (this.difficulty) {
             case "easy":
                 this.h4.textContent = "Mode Facile";
-                this.divEasy.className = "font-weight-bold";
-                this.divMedium.className = "";
-                this.divHard.className = "";
+                this.divEasy.className = "col-4 text-center font-weight-bold";
+                this.divMedium.className = "col-4 text-center";
+                this.divHard.className = "col-4 text-center";
+                this.getScore(6);
                 break;
 
             case "medium":
                 this.h4.textContent = "Mode Moyen";
-                this.divEasy.className = "";
-                this.divMedium.className = "font-weight-bold";
-                this.divHard.className = "";
+                this.divEasy.className = "col-4 text-center";
+                this.divMedium.className = "col-4 text-center font-weight-bold";
+                this.divHard.className = "col-4 text-center";
+                this.getScore(12);
                 break;
 
             case "hard":
                 this.h4.textContent = "Mode Difficile";
-                this.divEasy.className = "";
-                this.divMedium.className = "";
-                this.divHard.className = "font-weight-bold";
+                this.divEasy.className = "col-4 text-center";
+                this.divMedium.className = "col-4 text-center";
+                this.divHard.className = "col-4 text-center font-weight-bold";
+                this.getScore(18);
                 break;
 
             default:
                 break;
         }
 
+
+    }
+
+    getScore($id){
+        $.get('/Memory/mooc-symfony4/public/index.php/score/getscore/'+$id, function(data){
+            document.querySelector("tbody").innerHTML = '';
+            for (let i=0; i <= data.length; i++){
+                this.tr = document.createElement('tr');
+                this.tr.innerHTML = '<td>' + data[i].player + '</td><td>' + data[i].try + '</td><td>' + data[i].time + '</td>';
+                document.querySelector("tbody").append(this.tr);
+            }
+        });
     }
 }
 
