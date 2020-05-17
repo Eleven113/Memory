4e809577a12759c8a41a5c065731415fbd38f7c5
Khornya <marine.khornya@gmail.com>
2020-05-17 17:00:33 +0200

    Update HighscoreDisplay.js

--- public/js/HighscoreDisplay.js
+++ public/js/HighscoreDisplay.js
@@ -1,4 +1,4 @@
-class HighscoreDisplay{
+class HighscoreDisplay {
     constructor() {
         this.divEasy = document.getElementById("hs_difficulty-easy");
         this.divMedium = document.getElementById("hs_difficulty-medium");
@@ -12,64 +12,69 @@ class HighscoreDisplay{
         this.events();
     }
 
-    events(){
-        this.divEasy.addEventListener("click", function(event){
+    events() {
+        this.divEasy.addEventListener("click", function (event) {
             this.difficulty = "easy";
             this.display(event);
         }.bind(this));
 
-        this.divMedium.addEventListener("click", function(event){
+        this.divMedium.addEventListener("click", function (event) {
             this.difficulty = "medium";
             this.display(event);
         }.bind(this));
 
-        this.divHard.addEventListener("click", function(event){
+        this.divHard.addEventListener("click", function (event) {
             this.difficulty = "hard";
             this.display(event);
         }.bind(this));
 
-        this.div1Player.addEventListener("click", function(event){
+        this.div1Player.addEventListener("click", function (event) {
             this.numplayers = 1;
             this.display(event);
         }.bind(this));
 
-        this.div2Players.addEventListener("click", function(event){
+        this.div2Players.addEventListener("click", function (event) {
             this.numplayers = 2;
             this.display(event);
         }.bind(this));
 
     }
 
-    display(event){
+    display(event) {
         let difficulties = {
             "easy": "facile",
             "medium": "moyen",
             "hard": "difficile"
         };
         let numplayers = {
-          1: "1 joueur",
-          2: "2 joueurs"
+            1: "1 joueur",
+            2: "2 joueurs"
         };
-        let divs = {
-          "easy": this.divEasy,
-          "medium": this.divMedium,
-          "hard": this.divHard,
-          "1": this.div1Player,
-          "2": this.div2Players
+        let difficultyDivs = {
+            "easy": this.divEasy,
+            "medium": this.divMedium,
+            "hard": this.divHard
+        }
+        let numPlayerDivs = {
+            "1": this.div1Player,
+            "2": this.div2Players
         };
         this.h4.textContent = "Mode " + numplayers[this.numplayers] + " - " + difficulties[this.difficulty];
-        for (let div of Object.values(divs)) {
-            div.className = "col-4 text-center";
+        for (let div of Object.values(difficultyDivs)) {
+            div.className = "hs_menu col-4 text-center";
         }
-        divs[this.difficulty].className += " font-weight-bold";
-        divs[this.numplayers].className += " font-weight-bold";
+        for (let div of Object.values(numPlayerDivs)) {
+            div.className = "hs_menu col-3 text-center";
+        }
+        difficultyDivs[this.difficulty].className += " font-weight-bold";
+        numPlayerDivs[this.numplayers].className += " font-weight-bold";
         this.getScore(this.difficulty, this.numplayers);
     }
 
-    getScore($numcards,$numplayers){
-        $.get('/Memory/public/index.php/score/getscore/'+$numcards+'/'+$numplayers, function(data){
+    getScore($numcards, $numplayers) {
+        $.get('/Memory/public/index.php/score/getscore/' + $numcards + '/' + $numplayers, function (data) {
             document.querySelector("tbody").innerHTML = '';
-            for (let i=0; i <= data.length; i++){
+            for (let i = 0; i <= data.length; i++) {
                 this.tr = document.createElement('tr');
                 this.tr.innerHTML = '<td>' + data[i].player + '</td><td>' + data[i].try + '</td><td>' + data[i].time + '</td>';
                 document.querySelector("tbody").append(this.tr);
